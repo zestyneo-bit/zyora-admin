97d9028cafccb5f792bcd54e53cbafb24f46b1f2
zestyneo-bit <zestyneo@gmail.com>
2026-01-09 19:48:21 +0530

    Fix Firebase config + storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,24 +1,26 @@
 // src/firebase/config.js
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Your Firebase project config
 const firebaseConfig = {
   apiKey: "AIzaSyBF6wac1nEgBFwEWh5rVE2w0gIs2PWfnNY",
   authDomain: "zyora-86720.firebaseapp.com",
   projectId: "zyora-86720",
-  storageBucket: "zyora-86720.firebaseapp.com",
+  storageBucket: "zyora-86720.appspot.com",
   messagingSenderId: "382434590728",
   appId: "1:382434590728:web:c9fa2f938872f5d4fcd1bd",
   measurementId: "G-8C1PT5ZCK3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// Export services
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
