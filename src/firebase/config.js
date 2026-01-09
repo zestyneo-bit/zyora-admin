8b26992d32ce31738b7617a38481febf2a83647e
zestyneo-bit <zestyneo@gmail.com>
2026-01-09 21:34:51 +0530

    Fix storage bucket

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,26 +1,26 @@
-// src/firebase/config.js
+// zyora-admin/src/firebase/config.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Correct Firebase config
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBF6wac1nEgBFwEWh5rVE2w0gIs2PWfnNY",
   authDomain: "zyora-86720.firebaseapp.com",
   projectId: "zyora-86720",
-  storageBucket: "zyora-86720.appspot.com",
+  storageBucket: "zyora-86720.firebasestorage.app",
   messagingSenderId: "382434590728",
-  appId: "1:382434590728:web:c9fa2f938872f5d4fcd1bd",
-  measurementId: "G-8C1PT5ZCK3"
+  appId: "1:382434590728:web:c9fa2f938872f5d4fcd1bd"
 };
 
-// Init Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
 
-export default app;
+// IMPORTANT: force correct bucket or uploads fail
+export const storage = getStorage(app, "gs://zyora-86720.firebasestorage.app");
